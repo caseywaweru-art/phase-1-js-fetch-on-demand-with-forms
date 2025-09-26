6157de0368f76baf4d974a621264fc79ad7fd4ac
Casey Waweru <casey.waweru@gmail.com>
2025-09-26 19:49:39 +0300

    Learning to fetch with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID").value;
+
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((response) => response.json())
+      .then((data) => {
+	appendData(data);
+      })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function appendData(form) {
+  const title = document.querySelector("section#movieDetails h4");
+  const summary = document.querySelector("section#movieDetails p");
+
+  title.innerText = form.title;
+  summary.innerText = form.summary;
+}
